4e52252028627bc1eaf2e429c5529bc86fc54ab8
hp <teddybear90@abv.bg>
2018-08-20 15:34:05 +0300

    current user

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '../../../node_modules/angularfire2/auth';
+import { Component, OnInit } from "@angular/core";
+import { AngularFireAuth } from "../../../node_modules/angularfire2/auth";
+import * as firebase from "firebase"
 
 @Component({
-  selector: 'app-bs-navbar',
-  templateUrl: './bs-navbar.component.html',
-  styleUrls: ['./bs-navbar.component.css']
+  selector: "app-bs-navbar",
+  templateUrl: "./bs-navbar.component.html",
+  styleUrls: ["./bs-navbar.component.css"]
 })
-export class BsNavbarComponent  {
+export class BsNavbarComponent {
+  user: firebase.User;
 
-  constructor( private afAuth:AngularFireAuth) { 
-    afAuth.authState.subscribe(x=>console.log(x))
+  constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user => this.user=user);
+   
   }
 
- 
-
-  logout(){
-  this.afAuth.auth.signOut()
-
+  logout() {
+    this.afAuth.auth.signOut();
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,7 +11,7 @@ export class LoginComponent {
   constructor(private afAuth: AngularFireAuth) {}
   login() {
     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
- 
+    
   }
 
  
