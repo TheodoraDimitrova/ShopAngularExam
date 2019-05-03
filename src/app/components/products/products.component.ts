d3ca66ea2a704c10035e861a88de97a3a2dde50f
TheodoraDimitrova <teddybear90@abv.bg>
2019-05-03 20:15:57 +0300

    udating

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { ProductService } from "../../services/product.service";
 import { CategoryService } from "../../services/category.service";
 import { ActivatedRoute } from "../../../../node_modules/@angular/router";
 import { ShoppingCartService } from "../../services/shopping-cart.service";
+import { take } from "rxjs/operators";
 
 @Component({
   selector: "app-products",
   templateUrl: "./products.component.html",
   styleUrls: ["./products.component.css"]
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
   products: any[];
   categories$;
   category: string;
   filteredProducts: any[];
-
+  
 
   constructor(
     route: ActivatedRoute,
@@ -24,28 +25,39 @@ export class ProductsComponent implements OnInit {
   ) {
     productService.getAll().subscribe(p => {
       this.products = p;
-      console.log(this.products)
+      console.log(this.products);
       this.categories$ = categoryService.getCategories();
-
       route.queryParamMap.subscribe(params => {
         this.category = params.get("category");
         this.filteredProducts = this.category
-          ? this.products.filter(p => p.category === this.category)
+          ? this.products.filter(p => p.data.category === this.category)
           : this.products;
       });
     });
   }
 
-
-
-
   addToCart(product) {
-    this.cardService.addToCart(product)
+    this.cardService.addToCart(product);
   }
-
- 
-
-  ngOnInit() {
+  getQuantity(productKey) {
+    let cartId = localStorage.getItem("cartId");
     
+    if (cartId) {
+      this.cardService
+        .getItem(cartId, productKey)
+        .snapshotChanges()
+        .pipe(take(1))
+        .subscribe((i: any) => {
+         return i.payload.val().quantity;
+        });
+        
+    }else{
+      return 0
+    }
+  
+
+    
+   
   }
+ 
 }
--- src/app/components/shopping-card/shopping-card.component.ts
+++ src/app/components/shopping-card/shopping-card.component.ts
@@ -1,40 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { ShoppingCartService } from '../../services/shopping-cart.service';
-import { map } from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { ShoppingCartService } from "../../services/shopping-cart.service";
 import { take } from "rxjs/operators";
 
-
 @Component({
-  selector: 'app-shopping-card',
-  templateUrl: './shopping-card.component.html',
-  styleUrls: ['./shopping-card.component.css']
+  selector: "app-shopping-card",
+  templateUrl: "./shopping-card.component.html",
+  styleUrls: ["./shopping-card.component.css"]
 })
 export class ShoppingCardComponent implements OnInit {
-    cartId:string
-    cart;
-    items:any[];
-  dbTotal: any;
-  dbCartCount: number;
-  constructor(private shoppingCardService:ShoppingCartService) { }
+  cartId: string;
+  cart:{};
+  items: any[];
+  constructor(private shoppingCardService: ShoppingCartService) {}
 
   ngOnInit() {
+    this.cartId = localStorage.getItem("cartId");
+     this.shoppingCardService
+      .getCart(this.cartId)
+      .pipe(take(1))
+      .subscribe((i: any) => {
+        if( i.payload.val()){
+          this.cart = i.payload.val();
+           console.log(this.cart)
+          this.items = Object.keys(i.payload.val().items).map(key => ({
+            key: key,
+            value: i.payload.val().items[key]
+          }));
+        }
+        
+      });
+  }
 
-      this.cartId=localStorage.getItem("cartId")
-    this.cart=this.shoppingCardService.getCart(this.cartId)
-    .pipe(take(1))
-    .subscribe((i:any) => {
-      this.cart=i.payload.val()
-     
-      this.items=Object.keys(i.payload.val().items)
-      .map(key => ({key: key, value:i.payload.val().items[key]}));
-    console.log((this.items))
-      
-    });
-
-  } 
-
-
-  
-  
-
+  clearCart() {
+    this.shoppingCardService.clearCart();
+    this.cart={}
+  }
+ 
+ 
 }
